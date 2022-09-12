b442c4b186884e5a74d641e681cb2b7075f38a4c
sashibalijepalli <113393390+sashibalijepalli@users.noreply.github.com>
2022-09-12 18:20:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title("My Parent's new healhty diner")
 
 streamlit.header('Breakfast Menu')
+
+streamlit.text("Oats idly")
+streamlit.text("Kale, Spinach and blueberry smoothie")
+streamlit.text("Omega 3 infused Chai tea")
